58770371ccad0b9fb0062298c97db925c8ec4142
Jordan Long <jordanleelong5@gmail.com>
2019-10-08 02:57:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,7 +163,7 @@ def player_numbers(team_name)
         if attribute == :players
           data.each do |player|
             player[:number]
-            end
+          end
         end
       end
     end
