62b61effe788e0d2960209f102d9dadaf0619e04
Jordan Long <jordanleelong5@gmail.com>
2019-10-08 03:13:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -175,7 +175,7 @@ end
 def player_stats(players_name)
   new_hash = {}
   game_hash.each do |place, team|
-    team.each do |attribute, data|
+    team.each do |attributes, data|
       if attributes == :players
         data.each do |player|
           if player[:player_name] == players_name
