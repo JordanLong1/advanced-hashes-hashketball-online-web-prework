62a0d4576eab0012280191d0a73537b4562d27eb
Jordan Long <jordanleelong5@gmail.com>
2019-10-08 02:28:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,7 +119,7 @@ def num_points_scored(players_name)
     team.each do |attribute, data|
       if attribute == :players
         data.each do |player|
-          if player[:player_name] == players_name
+          if player[:player_name] == player_name
             return player[:points]
           end
         end
