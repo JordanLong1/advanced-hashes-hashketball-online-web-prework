38ff0a53cd5260368a02172da7c8494cb3a02d48
Jordan Long <jordanleelong5@gmail.com>
2019-10-08 02:56:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -161,7 +161,8 @@ def player_numbers(team_name)
     if team[:team_name] == team_name
       team.each do |attribute, data|
         if attribute == :players
-          return data[:number]
+
+        end
         end
       end
     end
